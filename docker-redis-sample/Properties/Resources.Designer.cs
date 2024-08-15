4601907a4b1762784c67cffd2c40b3e468383c78
Diego Balduino <homertrix_x@hotmail.com>
2024-08-15 15:25:54 -0300

    refactor: ajustes de organização de pasta

--- docker-redis-sample/Properties/Resources.Designer.cs
+++ docker-redis-sample/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Docker.Redis.Sample.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Docker.Redis.Sample.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("docker-redis-sample.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
